9dde47adfdd68b7956002710d30e759161677093
devgembaware <dev@gembaware.com>
2024-05-27 10:42:07 +0200

    on-send console log func

--- extension/src/taskpane/onsend.tsx
+++ extension/src/taskpane/onsend.tsx
@@ -1,12 +1,7 @@
 
-// @ts-ignore
-let mailboxItem: Office.Item & Office.ItemCompose & Office.ItemRead & Office.Message & Office.MessageCompose & Office.MessageRead & Office.Appointment & Office.AppointmentCompose & Office.AppointmentRead;
-
-Office.initialize = function () {
-    mailboxItem = Office.context.mailbox.item;
-
-}
-// @ts-ignore
-function loglogWhileSending(event: any) {
+function logWhileSending(event: any) {
     console.log('mail envoyé');
+    console.log(event);
 }
+
+console.log('file loaded');
