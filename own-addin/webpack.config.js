c0c88dc2691cb2aad0ccdb17d3c2063a0bce57d4
devgembaware <dev@gembaware.com>
2024-05-27 15:44:13 +0200

    test own-addin

--- own-addin/webpack.config.js
+++ own-addin/webpack.config.js
@@ -71,8 +71,8 @@ module.exports = async (env, options)  => {
               },
             },
             {
-              to: "launchevent.ts",
-              from: "./src/launchevent/launchevent.ts"
+              to: "launchevent.js",
+              from: "./src/launchevent/launchevent.js"
             },
           ]
         }
