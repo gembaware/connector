344c33838565e7cc6bbb4a4dd8c42e364ada6361
devgembaware <dev@gembaware.com>
2024-05-27 16:12:24 +0200

    test own-addin

--- own-addin/src/launchevent/launchevent.js
+++ own-addin/src/launchevent/launchevent.js
@@ -1,7 +1,12 @@
+import { Authenticator } from '@microsoft/office-js-helpers';
+
 function onMessageSendHandler(event) {
   console.log(event + "ok")
 }
 
-if (Office.context.platform === Office.PlatformType.PC || Office.context.platform === null) {
-  Office.actions.associate("onMessageSendHandler", onMessageSendHandler)
+Office.initialize = () => {
+  if (Authenticator.isAuthDialog()) return;
+  if (Office.context.platform === Office.PlatformType.PC || Office.context.platform === null) {
+    Office.actions.associate("onMessageSendHandler", onMessageSendHandler)
+  }
 }
