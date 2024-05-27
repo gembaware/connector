a48d57a91803cba04fa92566ee3646ef06a54f83
devgembaware <dev@gembaware.com>
2024-05-27 15:44:22 +0200

    own-addin onMessageSend

--- launchevent.ts
+++
@@ -1,7 +0,0 @@
-function onMessageSendHandler(event): void {
-  console.log(event + "ok")
-}
-
-if (Office.context.platform === Office.PlatformType.PC || Office.context.platform === null) {
-  Office.actions.associate("onMessageSendHandler", onMessageSendHandler)
-}
