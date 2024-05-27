a52837a397a9d25f9e1759718733c60a68993233
devgembaware <dev@gembaware.com>
2024-05-27 16:17:27 +0200

    own-addin onMessageSend

--- launchevent.js
+++ launchevent.js
@@ -1,11 +1,8 @@
-import { Authenticator } from '@microsoft/office-js-helpers';
-
 function onMessageSendHandler(event) {
   console.log(event + "ok")
 }
 
 Office.initialize = () => {
-  if (Authenticator.isAuthDialog()) return;
   if (Office.context.platform === Office.PlatformType.PC || Office.context.platform === null) {
     Office.actions.associate("onMessageSendHandler", onMessageSendHandler)
   }
