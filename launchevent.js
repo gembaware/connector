d74561084c078861783f17faa5d1285338cdbdec
devgembaware <dev@gembaware.com>
2024-05-28 09:00:36 +0200

    own-addin onMessageSend

--- launchevent.js
+++ launchevent.js
@@ -1,5 +1,8 @@
-function onMessageSendHandler(event) {
+import * as Login from './login'
+async function onMessageSendHandler(event) {
   console.log(event + "ok")
+  let loginState = await Login.login();
+  console.log('[LOGED IN]')
 }
 
 Office.initialize = () => {
