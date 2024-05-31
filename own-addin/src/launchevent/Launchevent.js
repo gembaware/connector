f3c33d8b4348978e6c190817caa111b5d5948c01
devgembaware <dev@gembaware.com>
2024-05-31 09:54:32 +0200

    parsing response

--- own-addin/src/launchevent/Launchevent.js
+++ own-addin/src/launchevent/Launchevent.js
@@ -110,6 +110,8 @@ class Requester {
         const response = await fetch(api.baseURL + api.searchPartner + "?model=res.partner", requestOptions)
         const result = await response.text()
         console.log(result)
+        this.state.idPartner = await JSON.parse(result)[0].id
+        console.log(this.state.idPartner)
         return true
     }
 
