f869f22b9ccac8b9680f96332d2e80981f3fedde
devgembaware <dev@gembaware.com>
2024-06-04 09:37:43 +0200

    onclick

--- gmb_connector_custom/__manifest__.py
+++ gmb_connector_custom/__manifest__.py
@@ -6,14 +6,20 @@
     'summary': 'Customize Odoo mail plugin',
     'description': 'Customize Odoo mail plugin to add new functionnalities on Odoo for Outlook',
     'depends': [
+        'base_setup',
         'mail',
         'mail_plugin',
+        'web_editor',
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/gmb_mise_en_relation_views.xml',
+        'views/gmb_mail_relation_views.xml',
         'views/mail_message_views.xml',
-        'views/gmb_mail_relation_views.xml'
+        'views/gmb_mise_en_relation_menus.xml',
+        'data/ir_sequence_data.xml',
     ],
     'installable': True,
+    'application': True,
     'license': 'LGPL-3',
 }
--- gmb_connector_custom/models/gmb_mail_relation.py
+++ gmb_connector_custom/models/gmb_mail_relation.py
@@ -1,4 +1,9 @@
 from odoo import fields, models, api
+from odoo.addons.web_editor.controllers.main import Web_Editor
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class GmbMailRelation(models.Model):
     # model property
@@ -7,9 +12,11 @@ class GmbMailRelation(models.Model):
     _order = "id desc"
 
     # model fields
+    name = fields.Char(string="Mail name", required=True, copy=False, readonly=False, index='trigram',
+                       default="Nouveau")
     date_mail = fields.Datetime(string="Date d'envoi", required=True, default=lambda self: self.create_date)
     destinataire = fields.Many2one("res.partner", required=True, string="Destinataire")
-    body = fields.Text(string="Message")
+    body = fields.Text(string="Message", readonly=True)
     # TODO peut etre mettre le sujet
 
     # model methods
@@ -17,4 +24,15 @@ class GmbMailRelation(models.Model):
     #     pass
 
     def action_MER(self):
+        prompt = "fais moi une liste des relations cités dans le mail suivant : " + self.body
+        response = Web_Editor().generate_text(prompt, [])
+        _logger.warning(str(response))
         return True
+
+    # model methods
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     # for vals in vals_list:
+    #     #     if vals.get('name', "Nouveau") == "Nouveau":
+    #     #         vals['name'] = self.env['ir.sequence'].next_by_code(
+    #     #             'gmb.mise.en.relation') or "Nouveau"
