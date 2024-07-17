d0bd3e16862535aa5c02ce2cd733d59577cefd72
devgembaware <dev@gembaware.com>
2024-07-17 10:09:12 +0200

    [IMP] rest_api_odoo : fix request.env

--- rest_api_odoo/controllers/rest_api_odoo.py
+++ rest_api_odoo/controllers/rest_api_odoo.py
@@ -266,7 +266,7 @@ class RestApi(http.Controller):
                     return (f"<html><body><h3>No report {report_id} for model {model.id}</h3></body></html>")
             else:
                 report = request.env['ir.actions.report'].search([('model', '=', model.name)], limit=1)
-            _logger.warning("test + " + str(report.report_file))
+            _logger.warning("test + " + str(report.report_type))
             # TODO : appel de impression
 
 
